fbe9f9286f54b2402e76c2ae07b8160ff4911e1b
Petrisor Alex <alex_petrisor2001@yahoo.com>
2021-02-21 16:43:47 +0200

    DialogBox for Income

--- app/src/main/java/com/example/expenseapp/DashboardFragment.java
+++ app/src/main/java/com/example/expenseapp/DashboardFragment.java
@@ -1,18 +1,25 @@
 package com.example.expenseapp;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.transition.Fade;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class DashboardFragment extends Fragment {
 
@@ -32,11 +39,28 @@ public class DashboardFragment extends Fragment {
     //animation
     private Animation FadeOpen,FadeClose;
 
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+    private DatabaseReference mExpenseDatabase;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myview= inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+
+        mAuth=FirebaseAuth.getInstance();
+
+        FirebaseUser mUser=mAuth.getCurrentUser();
+
+        String uid=mUser.getUid();
+        mIncomeDatabase= FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+        mIncomeDatabase= FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+
 
         //connect floating button to layout
         fab_main_btn=myview.findViewById(R.id.fb_main_plus_btn);
@@ -55,6 +79,8 @@ public class DashboardFragment extends Fragment {
         fab_main_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                addData();
 
                 if(isopen){
                     fab_income_btn.startAnimation(FadeClose);
@@ -89,4 +115,86 @@ public class DashboardFragment extends Fragment {
 
         return myview;
     }
+
+
+    private void addData(){
+
+        //fab Button income
+        fab_income_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incomeDataInsert();
+
+            }
+        });
+
+        fab_expense_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+    }
+
+
+    public void incomeDataInsert(){
+
+        AlertDialog.Builder mydialog=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=LayoutInflater.from(getActivity());
+        View myview=inflater.inflate(R.layout.custom_layout_for_insertdata,null);
+        mydialog.setView(myview);
+
+        AlertDialog dialog=mydialog.create();
+
+        EditText editAmount=myview.findViewById(R.id.amount_edit);
+        EditText editType=myview.findViewById(R.id.type_edit);
+        EditText editNote=myview.findViewById(R.id.note_edit);
+
+        Button btnSave=myview.findViewById(R.id.btnSave);
+        Button btnCancel=myview.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String type=editType.getText().toString().trim();
+                String amount=editAmount.getText().toString().trim();
+                String note=editNote.getText().toString().trim();
+
+                if(TextUtils.isEmpty(type)){
+                    editType.setError("Required Field...");
+                    return;
+                }
+                if(TextUtils.isEmpty(amount)){
+                    editAmount.setError("Required Field...");
+                    return;
+                }
+
+                int ouramountint=Integer.parseInt(amount);
+
+
+
+                if(TextUtils.isEmpty(note)){
+                    editNote.setError("Required Field...");
+                    return;
+                }
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+
+
+
 }
